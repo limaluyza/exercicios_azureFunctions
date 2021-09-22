915feb151a9235af8c15953f11a79209892ff946
limaluyza <luyza2129@gmail.com>
2021-09-22 17:09:23 -0300

    segundo exercicio

--- src/main/java/com/function/exercicio2.java
+++ src/main/java/com/function/exercicio2.java
@@ -4,20 +4,12 @@ import java.util.*;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
-public class Exercicio1 {
-    /**
-     * This function listens at endpoint "/api/Exercicio1". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/Exercicio1
-     * 2. curl {your host}/api/Exercicio1?name=HTTP%20Query
-     */
-    @FunctionName("Exercicio1")
+public class exercicio2 {
+    @FunctionName("Exercicio2")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", 
-            methods = {HttpMethod.GET},
-            route= "exercicio1/{valor}",
+            methods = {HttpMethod.POST},
+            route= "exercicio2/{valor}",
              authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             @BindingName("valor") Integer valor, 
              final
@@ -36,3 +28,4 @@ public class Exercicio1 {
         }
     }
 }
+    
--- src/main/java/com/function/exerciciojava2.java
+++
@@ -1,5 +0,0 @@
-package com.function;
-
-public class exerciciojava2 {
-    
-}
